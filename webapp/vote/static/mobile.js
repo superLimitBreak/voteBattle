5999176157f0615b46a856651ab7d2ed1d55d89b
Allan Callaghan <calaldees@hotmail.com>
2014-01-24 07:49:57 +0000

    Mobile client buttons disbale on vote

--- webapp/vote/static/mobile.js
+++ webapp/vote/static/mobile.js
@@ -25,33 +25,54 @@ function now() {
 };
 now();  // Set cookies and init server offset as soon as possible
 
-// Startup ---------------------------------------------------------------------
+// utils ------
 
-function startup_client(pool_id) {
+function set_vote_input_state(state) {
+    console.log("set_vote_input_state", state)
+    $('#vote_input li button').each(function(i, element){
+        var $element = $(element);
+        $element.attr('disabled', !state);
+    });
+}
+
+// Flow ------------------------------------------------------------------------
+
+function get_frame(pool_id) {
+    console.debug("get_frame", pool_id);
 	$.getJSON('/api/'+pool_id+'.json')
 	.success(function(response_json){
         var votes = response_json['data']['frame']['votes'];
-        console.debug("vote_options", votes);
-        var $vote_input = $('#vote_input');
-        $vote_input.empty();
-		$.each(votes, function(key, value){
-            $vote_input.append('<li data-item="'+key+'">'+key+'</li>');
-        });
-        $vote_input.find('li').on('click', function(){
-            var item = $(this).data('item');
-            //console.debug("vote", item);
-            $.getJSON('/api/'+pool_id+'/vote.json?item='+item)
-            .success(function(data){
-                console.debug("vote successful");
-                $vote_input.empty();
-            })
-            .error(function(xhr){
-                console.error(xhr.responseJSON['messages'][0]);
-            });
-        });
+        setup_vote_input(pool_id, votes);
 	})
 	.error(function(xhr){
         console.error("startup_client failed");
 	});
+}
 
+function setup_vote_input(pool_id, votes) {
+    console.debug("setup_vote_input", votes)
+    var $vote_input = $('#vote_input');
+    $vote_input.empty();
+    $.each(votes, function(key, value){
+        $vote_input.append('<li><button data-item="'+key+'">'+key+'</button></li>');
+    });
+    $vote_input.find('li button').on('click', function(){
+        set_vote_input_state(false);
+        var item = $(this).data('item');
+        //console.debug("vote", item);
+        $.getJSON('/api/'+pool_id+'/vote.json?item='+item)
+        .success(function(data){
+            console.debug("vote successful");
+            $vote_input.empty();
+        })
+        .error(function(xhr){
+            console.error(xhr.responseJSON['messages'][0]);
+        });
+    });
+}
+
+// Startup ---------------------------------------------------------------------
+
+function startup_client(pool_id) {
+    get_frame(pool_id);
 };

aed8b7cc1fa85964fa24e3b9d50496a113ca22a3
Allan Callaghan <calaldees@gmail.com>
2015-01-13 18:51:29 +0000

    Added postmessage support for triggering preroll

---
+++ server/vote/static/projector/js/postmessage_handler.js
@@ -0,0 +1,17 @@
+
+window.addEventListener('message', function(event) {
+    if (event.origin.indexOf('localhost') == -1) {console.error('postMessage with incorrect origin', event.origin); return;}
+    var data = JSON.parse(event.data);
+    
+	if (data.func_iframe) {
+		battlescape.ui.screen(data.func_iframe);
+	}
+	
+	//event.source.postMessage('reply', event.origin);  // post back to origin
+},false);
+
+console.log("postmessage handlers set")
+
+function test_me() {
+	alert("test");
+}

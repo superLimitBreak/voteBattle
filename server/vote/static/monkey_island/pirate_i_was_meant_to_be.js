f49e42546a131694d1091edef19d38553eff8927
Allan Callaghan <calaldees@gmail.com>
2015-06-24 08:00:18 +0100

    Considered monkey island dialog voting

---
+++ server/vote/static/monkey_island/pirate_i_was_meant_to_be.js
@@ -0,0 +1,114 @@
+A Pirate I Was Meant To Be
+by Tim Schafer, Lead Designer of "The Curse of Monkey Island"
+
+(This one requires four men and tells the tale of a Captain that wanted his lazy crew to help him recover a stolen treasure map, but the crew would much rather stay on decks singing. What makes things worse is that every time the Captain says anything, the mutinous crew just rhymes it to make it part of their song)
+
+(Talking)
+Captain: Thanks for the help back there guys.(sarcastically)
+Third Mate: It was a rousing battle, Captain.
+First Mate: Aye, and it reminds me of a song...
+
+(singing)
+First Mate: We're a band of vicous pirates
+Second Mate: A Sailin' Out to Sea!
+Third Mate: When ye hear our gentle singin'
+First Mate: Be sure to turn and flee!
+(talking) Captain: Ohh, this is just ridiculous.
+
+---
+
+Captain: Come on men, We've GOT to recover that map!
+Second Mate: That pirate will be done for when he falls into our trap!
+
+Third Mate: We're a club of tuneful rovers
+First Mate: We can sing at every clef
+Second Mate: We can even hit the high notes!
+First Mate: It's just too bad we're tone deaf.
+
+All Mates: A pirate I was meant to be, trim the sails and roam the seas!
+
+---
+
+Captain: Let's go defeat that evil pirate!
+Second Mate: We know he's sure to lose because we know just where to fire at!
+
+Second Mate: We're a thieving valideer
+First Mate: A gang of cutthroat mugs
+Third Mate: To fight us off you wont need guns
+Second Mate: Just jolly good earplugs!
+
+All Mates: A pirate I was meant to be, trim the sails and roam the seas!
+
+---
+
+Captain: Alright crew, let's get to work!
+First Mate: Our vocation is a thing we love, a thing we never shirk!
+
+First Mate: We'll fightchu in the harbor
+Third Mate: We'll battle you on land
+Second Mate: But when you meet singing pirates
+(talking) Captain: They'll be more than you can stand.
+First Mate: Oh, ho, that was a good one!
+Captain: No, it wasn't.
+
+---
+
+Captain: No time for song, we've got to move!
+Third Mate: The battle will be long, but our courage we will prove!
+
+Second Mate: We're a pack of scurvy seadogs
+First Mate: Have we pity? Not a gram
+Third Mate: We all eat roasted garlic
+First Mate: And sing from the diaphram.c
+
+All Mates: A pirate I was meant to be, trim the sails and roam the seas!
+
+---
+
+Captain: Less Singing, More Sailing!
+Second Mate: When we defeat our wicked show his ship he will be bailing!
+
+Third Mate: If ye try to fight us
+First Mate: You'll get a nasty whackin'
+Second Mate: If you disrespect our singin'
+First Mate: We will bring ye to a crackin'!
+
+All Mates: A pirate I was meant to be, trim the sails and roam the seas!
+
+---
+
+Captain: I'm getting so sick of you guys and your rhyming!
+First Mate: We're ready to set sail, though the cannons need a primin'!
+
+Second Mate: We're troublesome corsairs
+Third Mate: We've come to steal yer treasure
+First Mate: We would shoot ya in the dungbie
+Second Mate: But we've got a rest by measures
+
+All Mates: A pirate I was meant to be, trim the sails and roam the seas!
+
+---
+
+Captain: (whining)Stop, stop, stop!
+Third Mate: The brass is what we'll pollish and the deck is what we'll mop!
+
+Captain: You say you're nasty pirates,
+Thieving scheming bad-bush whackers,
+But from what I've seen I tell you you're no pirates,
+You're just slackers!
+All Mates: A pirate I was meant to be, trim the sails and roam the seas!
+
+---
+
+Captain: We'll surely avoid scurvy if we all eat an orange.
+(Crew Desperately tries to think of a word to rhyme orange with)
+First Mate: and..
+Second Mate: umm...
+Third Mate: well..
+First Mate: uhh..
+Third Mate: Door Hinge?
+Second Mate: No, no..
+Third Mate: Guess the song's over then.
+First Mate: Guess so...
+Second Mate: OK, back to work.
+Captain: Well, gee.. I feel a little guilty now.
